5d4e5d3f8630608b6feab2462753965a98fb3f73
Gabriella-C <gabriella.cristinec@gmail.com>
2021-03-17 10:24:41 -0300

    criando controller, view, e mandando informacoes para as view

---
+++ src/main/java/br/com/mudi/mvc/mudi/controller/HelloController.java
@@ -0,0 +1,17 @@
+package br.com.mudi.mvc.mudi.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello(Model model) {
+		model.addAttribute("nome", "mundo");
+		return "hello";
+	}
+}
