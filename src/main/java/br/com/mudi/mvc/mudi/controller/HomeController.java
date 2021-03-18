9c55f36e23c6f389f4a86d8b39bd37cec50646a3
Gabriella-C <gabriella.cristinec@gmail.com>
2021-03-18 15:01:18 -0300

    spring data com jpa, usando  reporitory e jparepository

---
+++ src/main/java/br/com/mudi/mvc/mudi/controller/HomeController.java
@@ -0,0 +1,26 @@
+package br.com.mudi.mvc.mudi.controller;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.mudi.mvc.mudi.model.Pedido;
+import br.com.mudi.mvc.mudi.repository.PedidoRepository;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@GetMapping("/home")
+	public String home(Model model) {
+		List<Pedido> pedidos = pedidoRepository.findAll();
+		model.addAttribute("pedidos", pedidos);
+		return "home";
+	}
+} 
---
+++ src/main/java/br/com/mudi/mvc/mudi/model/Pedido.java
@@ -0,0 +1,63 @@
+package br.com.mudi.mvc.mudi.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+ 
+@Entity
+public class Pedido {
+	
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private Long id;
+	
+	private String nomeProduto;
+	private BigDecimal valorNegociado;
+	private LocalDate dataDaEntrega;
+	
+	private String urlProduto;
+	private String urlImagem;
+	private String Descricao;
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public BigDecimal getValorNegociado() {
+		return valorNegociado;
+	}
+	public void setValorNegociado(BigDecimal valorNegociado) {
+		this.valorNegociado = valorNegociado;
+	}
+	public LocalDate getDataDaEntrega() {
+		return dataDaEntrega;
+	}
+	public void setDataDaEntrega(LocalDate dataDaEntrega) {
+		this.dataDaEntrega = dataDaEntrega;
+	}
+	public String getUrlProduto() {
+		return urlProduto;
+	}
+	public void setUrlProduto(String urlProduto) {
+		this.urlProduto = urlProduto;
+	}
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+	public String getDescricao() {
+		return Descricao;
+	}
+	public void setDescricao(String descricao) {
+		Descricao = descricao;
+	}
+	
+}
